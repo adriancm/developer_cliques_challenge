1833c97de61cd5201ad0672c9696d929fef78c54
Adrian Cepillo <adrian.cepillo@gmail.com>
2018-09-28 15:06:20 +0200

    Adds max_cliques method to Graph class using Bron-Kerbosch algorithm with pivoting

--- lib/graph.rb
+++ lib/graph.rb
@@ -9,7 +9,37 @@ class Graph
   end
 
   def neighbours node:
-    @edges[node]
+    @edges[node] || []
+  end
+
+  def max_cliques
+    @cliques ||= []
+    bron_kerbosch(possibles: nodes) if @cliques.empty?
+    @cliques
+  end
+
+  private
+
+  # Bron-Kerbosch Algorithm with Pivoting
+  # @param result
+  # is the set with the nodes of a maximal clique
+  # @param possibles
+  # is the set of the possible nodes to look
+  # @param excluded
+  # is the set of the excluded nodes
+  def bron_kerbosch result: [], possibles: [], excluded: []
+    @cliques << result if possibles.empty? and excluded.empty?
+
+    #Pivoting: it takes first element of possibles as pivot
+    (possibles - neighbours(node: possibles.first)).each do |n|
+      # Recursive call
+      bron_kerbosch result: result | [n],
+                    possibles: possibles & neighbours(node: n),
+                    excluded: excluded & neighbours(node: n)
+
+      possibles -= [n]
+      excluded |= [n]
+    end
   end
 
 end
--- spec/lib/graph_spec.rb
+++ spec/lib/graph_spec.rb
@@ -7,6 +7,7 @@ describe Graph do
   let(:edges) { { 1 => [2, 5], 2 => [1, 3, 5], 3 => [2, 4], 4 => [3, 5, 6], 5 => [1, 2, 4], 6 => [4] } }
   let(:nodes) { [1, 2, 3, 4, 5, 6] }
   let(:graph) { Graph.new edges: edges }
+  let(:cliques) { [[1,2,5], [3,2], [3,4], [4,5], [4,6]] }
 
   it 'returns edges' do
     expect(graph.edges).to eq(edges)
@@ -16,5 +17,19 @@ describe Graph do
     expect(graph.nodes).to eq(nodes)
   end
 
+  context 'neighbours' do
+    it 'returns correct nodes' do
+      expect(graph.neighbours(node: nodes.first)).to eq([2,5])
+    end
+
+    it 'returns empty set' do
+      expect(graph.neighbours(node: nil)).to eq([])
+    end
+  end
+
+
+  it 'returns maximal cliques' do
+    expect(graph.max_cliques).to eq(cliques)
+  end
 
 end
